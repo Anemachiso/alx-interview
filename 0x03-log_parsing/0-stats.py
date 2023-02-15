78a8353b5bfc51b39ec7c508ddfc05091406060c
Mulugeta Achiso <anem1351@gmail.com>
2023-02-15 02:02:24 -0800

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,59 @@
 #!/usr/bin/python3
-"""Performs log parsing from stdin"""
-
-import re
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
+"""
 import sys
-counter = 0
-file_size = 0
-statusC_counter = {200: 0, 301: 0, 400: 0,
-                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def printCodes(dict, file_s):
-    """Prints the status code and the number of times they appear"""
-    print("File size: {}".format(file_s))
-    for key in sorted(dict.keys()):
-        if statusC_counter[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
 if __name__ == "__main__":
+    cnt = 0
     try:
-        for line in sys.stdin:
-            split_string = re.split('- |"|"| " " ', str(line))
-            statusC_and_file_s = split_string[-1]
-            if counter != 0 and counter % 10 == 0:
-                printCodes(statusC_counter, file_size)
-            counter = counter + 1
+        """ Iter the standar input """
+        for data in sys.stdin:
             try:
-                statusC = int(statusC_and_file_s.split()[0])
-                f_size = int(statusC_and_file_s.split()[1])
-                # print("Status Code {} size {}".format(statusC, f_size))
-                if statusC in statusC_counter:
-                    statusC_counter[statusC] += 1
-                file_size = file_size + f_size
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
             except:
                 pass
-        printCodes(statusC_counter, file_size)
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
     except KeyboardInterrupt:
-        printCodes(statusC_counter, file_size)
+        prn_stats()
         raise
+    else:
+        prn_stats()
